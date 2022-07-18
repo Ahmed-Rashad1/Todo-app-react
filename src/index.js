213742c5ab2a82591dacc60e6eec68f8cf2af63c
a7med-rashad <a7medra4aad3@gmail.com>
2022-07-18 03:34:54 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = document.getElementById('root');
+render(<App />, root);
