a7cec5047788d9464a572a609c5e25637dd01c3c
ahmed rashad <104315358+a7med-rashad@users.noreply.github.com>
2022-07-24 19:24:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App/App';
 
-const root = document.getElementById('root');
-render(<App />, root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
